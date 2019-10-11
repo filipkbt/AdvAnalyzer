66da96d2c9bfa8af2cdef9d49d0b09346c6f9aa4
Filip Kubat <filip.kubat@wp.pl>
2019-10-11 19:50:54 +0200

    Add Core and Shared module, init sidenav

---
+++ AdvAnalyzer.WebApp/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+@NgModule({
+  declarations: [SidenavComponent],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatCheckboxModule
+  ]
+})
+export class SharedModule { }
