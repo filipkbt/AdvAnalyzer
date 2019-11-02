a1c96609a799864ff0c8edbe99a86ff0703aeab4
Filip Kubat <filip.kubat@wp.pl>
2019-11-02 17:49:58 +0100

    Add login component and model

--- AdvAnalyzer.WebApp/src/app/modules/auth/login/login.component.ts
+++ AdvAnalyzer.WebApp/src/app/modules/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/core/models/auth/login.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  model: Login = {
+    userName: '',
+    password: ''
+  };
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      'userName': ['', [Validators.required]],
+      'password': ['', [Validators.required]]
+    })
+  }
+
+  submit() {
+    this.model.userName = this.form.controls['userName'].value;
+    this.model.password = this.form.controls['password'].value;
   }
 
 }
