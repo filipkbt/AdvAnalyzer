aff11a97c76e8aca801903bc0d49c72c9b4cfa2e
Filip Kubat <filip.kubat@wp.pl>
2019-11-05 19:31:01 +0100

    Code refactor

--- AdvAnalyzer.WebApp/src/app/app.module.ts
+++ AdvAnalyzer.WebApp/src/app/app.module.ts
@@ -1,9 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
@@ -13,9 +11,7 @@ import { AuthModule } from './modules/auth/auth.module';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     SharedModule,
     CoreModule,
     AuthModule
--- AdvAnalyzer.WebApp/src/app/core/core.module.ts
+++ AdvAnalyzer.WebApp/src/app/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    BrowserModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    BrowserModule
   ]
 })
 export class CoreModule { }
