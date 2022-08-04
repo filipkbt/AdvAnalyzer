d3c3f17ff9c4372ea6134ac65d12880ba75f1a77
Filip <filip.kubat@wp.pl>
2022-08-04 23:09:35 +0200

    update dashboard module

--- AdvAnalyzer.WebApp/src/app/modules/dashboard/dashboard.module.ts
+++ AdvAnalyzer.WebApp/src/app/modules/dashboard/dashboard.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from 'src/app/shared/layout/layout.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardContainerComponent } from './containers/dashboard-container/dashboard-container.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent, children: [
+      { path: '', component: DashboardContainerComponent },
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [
-    DashboardContainerComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule
   ]
 })
 export class DashboardModule { }
