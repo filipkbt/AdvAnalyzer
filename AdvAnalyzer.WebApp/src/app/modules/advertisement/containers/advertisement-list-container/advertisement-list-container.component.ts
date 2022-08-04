4806297fe76b6e8431d32ad17799f0d3d30577db
Filip <filip.kubat@wp.pl>
2022-08-04 23:05:04 +0200

    finish notification-list component

--- AdvAnalyzer.WebApp/src/app/modules/advertisement/containers/advertisement-list-container/advertisement-list-container.component.ts
+++ AdvAnalyzer.WebApp/src/app/modules/advertisement/containers/advertisement-list-container/advertisement-list-container.component.ts
@@ -61,6 +61,6 @@ export class AdvertisementListContainerComponent implements OnInit {
 
   public setIsFavorite(advertisement: Advertisement): void {
     advertisement.isFavorite = !advertisement.isFavorite;
-    this.advertisementService.update(advertisement).pipe(take(1));
+    this.advertisementService.update(advertisement).pipe(take(1)).subscribe();
   }
 }
